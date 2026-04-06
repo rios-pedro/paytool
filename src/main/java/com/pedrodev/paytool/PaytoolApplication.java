c3b0323406d364fe3efbcad806ac75ac3c76c684
predohr <pedro.henrique9760@icloud.com>
2026-04-06 00:52:24 -0300

    Project created

---
+++ src/main/java/com/pedrodev/paytool/PaytoolApplication.java
@@ -0,0 +1,13 @@
+package com.pedrodev.paytool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaytoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pedrodev/paytool/PaytoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrodev.paytool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
