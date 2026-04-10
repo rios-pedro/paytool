bf57662ed5e694bfe8f63c138ffd39744dc51c68
predohr <pedro.henrique9760@icloud.com>
2026-04-10 15:38:45 -0300

    Service layer, component registration

---
+++ src/main/java/com/pedrodev/paytool/com/pedrodev/paytool/services/UserService.java
@@ -0,0 +1,25 @@
+package com.pedrodev.paytool.com.pedrodev.paytool.services;
+
+import com.pedrodev.paytool.entities.User;
+import com.pedrodev.paytool.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
